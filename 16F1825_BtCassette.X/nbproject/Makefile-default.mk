41b61b35f825498afcc9d97af89b3c25c7713b12
coderkevin <kevin@redfenix.com>
2013-12-20 23:07:41 -0600

    Update makefiles.

--- 16F1825_BtCassette.X/nbproject/Makefile-default.mk
+++ 16F1825_BtCassette.X/nbproject/Makefile-default.mk
@@ -45,17 +45,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=CM_Generated_Files/pin_manager.c CM_Generated_Files/mcc.c main.c CM_Generated_Files/interrupt_manager.c
+SOURCEFILES_QUOTED_IF_SPACED=CM_Generated_Files/pin_manager.c CM_Generated_Files/mcc.c CM_Generated_Files/interrupt_manager.c main.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/CM_Generated_Files/pin_manager.p1 ${OBJECTDIR}/CM_Generated_Files/mcc.p1 ${OBJECTDIR}/main.p1 ${OBJECTDIR}/CM_Generated_Files/interrupt_manager.p1
-POSSIBLE_DEPFILES=${OBJECTDIR}/CM_Generated_Files/pin_manager.p1.d ${OBJECTDIR}/CM_Generated_Files/mcc.p1.d ${OBJECTDIR}/main.p1.d ${OBJECTDIR}/CM_Generated_Files/interrupt_manager.p1.d
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/CM_Generated_Files/pin_manager.p1 ${OBJECTDIR}/CM_Generated_Files/mcc.p1 ${OBJECTDIR}/CM_Generated_Files/interrupt_manager.p1 ${OBJECTDIR}/main.p1
+POSSIBLE_DEPFILES=${OBJECTDIR}/CM_Generated_Files/pin_manager.p1.d ${OBJECTDIR}/CM_Generated_Files/mcc.p1.d ${OBJECTDIR}/CM_Generated_Files/interrupt_manager.p1.d ${OBJECTDIR}/main.p1.d
 
 # Object Files
-OBJECTFILES=${OBJECTDIR}/CM_Generated_Files/pin_manager.p1 ${OBJECTDIR}/CM_Generated_Files/mcc.p1 ${OBJECTDIR}/main.p1 ${OBJECTDIR}/CM_Generated_Files/interrupt_manager.p1
+OBJECTFILES=${OBJECTDIR}/CM_Generated_Files/pin_manager.p1 ${OBJECTDIR}/CM_Generated_Files/mcc.p1 ${OBJECTDIR}/CM_Generated_Files/interrupt_manager.p1 ${OBJECTDIR}/main.p1
 
 # Source Files
-SOURCEFILES=CM_Generated_Files/pin_manager.c CM_Generated_Files/mcc.c main.c CM_Generated_Files/interrupt_manager.c
+SOURCEFILES=CM_Generated_Files/pin_manager.c CM_Generated_Files/mcc.c CM_Generated_Files/interrupt_manager.c main.c
 
 
 CFLAGS=
@@ -94,14 +94,6 @@ ${OBJECTDIR}/CM_Generated_Files/mcc.p1: CM_Generated_Files/mcc.c  nbproject/Make
 	@-${MV} ${OBJECTDIR}/CM_Generated_Files/mcc.d ${OBJECTDIR}/CM_Generated_Files/mcc.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/CM_Generated_Files/mcc.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
-${OBJECTDIR}/main.p1: main.c  nbproject/Makefile-${CND_CONF}.mk
-	@${MKDIR} ${OBJECTDIR} 
-	@${RM} ${OBJECTDIR}/main.p1.d 
-	@${RM} ${OBJECTDIR}/main.p1 
-	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  -D__DEBUG=1 --debugger=icd3  --double=24 --float=24 --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 --warn=0 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,+osccal,-resetbits,-download,-stackcall,+clib --output=-mcof,+elf "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/main.p1  main.c 
-	@-${MV} ${OBJECTDIR}/main.d ${OBJECTDIR}/main.p1.d 
-	@${FIXDEPS} ${OBJECTDIR}/main.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
-	
 ${OBJECTDIR}/CM_Generated_Files/interrupt_manager.p1: CM_Generated_Files/interrupt_manager.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} ${OBJECTDIR}/CM_Generated_Files 
 	@${RM} ${OBJECTDIR}/CM_Generated_Files/interrupt_manager.p1.d 
@@ -109,6 +101,14 @@ ${OBJECTDIR}/CM_Generated_Files/interrupt_manager.p1: CM_Generated_Files/interru
 	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  -D__DEBUG=1 --debugger=icd3  --double=24 --float=24 --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 --warn=0 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,+osccal,-resetbits,-download,-stackcall,+clib --output=-mcof,+elf "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/CM_Generated_Files/interrupt_manager.p1  CM_Generated_Files/interrupt_manager.c 
 	@-${MV} ${OBJECTDIR}/CM_Generated_Files/interrupt_manager.d ${OBJECTDIR}/CM_Generated_Files/interrupt_manager.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/CM_Generated_Files/interrupt_manager.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
+${OBJECTDIR}/main.p1: main.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} ${OBJECTDIR} 
+	@${RM} ${OBJECTDIR}/main.p1.d 
+	@${RM} ${OBJECTDIR}/main.p1 
+	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  -D__DEBUG=1 --debugger=icd3  --double=24 --float=24 --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 --warn=0 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,+osccal,-resetbits,-download,-stackcall,+clib --output=-mcof,+elf "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/main.p1  main.c 
+	@-${MV} ${OBJECTDIR}/main.d ${OBJECTDIR}/main.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/main.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
 else
 ${OBJECTDIR}/CM_Generated_Files/pin_manager.p1: CM_Generated_Files/pin_manager.c  nbproject/Makefile-${CND_CONF}.mk
@@ -127,14 +127,6 @@ ${OBJECTDIR}/CM_Generated_Files/mcc.p1: CM_Generated_Files/mcc.c  nbproject/Make
 	@-${MV} ${OBJECTDIR}/CM_Generated_Files/mcc.d ${OBJECTDIR}/CM_Generated_Files/mcc.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/CM_Generated_Files/mcc.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
-${OBJECTDIR}/main.p1: main.c  nbproject/Makefile-${CND_CONF}.mk
-	@${MKDIR} ${OBJECTDIR} 
-	@${RM} ${OBJECTDIR}/main.p1.d 
-	@${RM} ${OBJECTDIR}/main.p1 
-	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  --double=24 --float=24 --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 --warn=0 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,+osccal,-resetbits,-download,-stackcall,+clib --output=-mcof,+elf "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/main.p1  main.c 
-	@-${MV} ${OBJECTDIR}/main.d ${OBJECTDIR}/main.p1.d 
-	@${FIXDEPS} ${OBJECTDIR}/main.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
-	
 ${OBJECTDIR}/CM_Generated_Files/interrupt_manager.p1: CM_Generated_Files/interrupt_manager.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} ${OBJECTDIR}/CM_Generated_Files 
 	@${RM} ${OBJECTDIR}/CM_Generated_Files/interrupt_manager.p1.d 
@@ -142,6 +134,14 @@ ${OBJECTDIR}/CM_Generated_Files/interrupt_manager.p1: CM_Generated_Files/interru
 	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  --double=24 --float=24 --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 --warn=0 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,+osccal,-resetbits,-download,-stackcall,+clib --output=-mcof,+elf "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/CM_Generated_Files/interrupt_manager.p1  CM_Generated_Files/interrupt_manager.c 
 	@-${MV} ${OBJECTDIR}/CM_Generated_Files/interrupt_manager.d ${OBJECTDIR}/CM_Generated_Files/interrupt_manager.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/CM_Generated_Files/interrupt_manager.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
+${OBJECTDIR}/main.p1: main.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} ${OBJECTDIR} 
+	@${RM} ${OBJECTDIR}/main.p1.d 
+	@${RM} ${OBJECTDIR}/main.p1 
+	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  --double=24 --float=24 --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 --warn=0 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,+osccal,-resetbits,-download,-stackcall,+clib --output=-mcof,+elf "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/main.p1  main.c 
+	@-${MV} ${OBJECTDIR}/main.d ${OBJECTDIR}/main.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/main.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
 endif
 
